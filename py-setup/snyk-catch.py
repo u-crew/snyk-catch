fed24e4c30b5bd915c0b7f97a0532290704cfe59
u-crew <129836422+u-crew@users.noreply.github.com>
2024-03-27 08:50:04 +1100

    debug

--- py-setup/snyk-catch.py
+++ py-setup/snyk-catch.py
@@ -4,23 +4,24 @@ from github_action_utils import event_payload
 # Get payload from the evnents check_run
 
 event_payload = event_payload()
-print(event_payload)
+
+print(event_payload['context'])
 # Check for snyk Open source license in pending state
-if event_payload['context'].startswith( "license/snyk") and event_payload['state']== 'pending':
-  # Report that 
-  print("Snyk Open source license check is in progress")
-  action(event_payload, "oss-license-scan")
-elif event_payload['context'].startswith( "security/snyk") and event_payload['state'] == 'pending':
-  # Report that 
-  print("Snyk Open source license check is in progress")
-  action(event_payload, "sca-scan")
-elif event_payload['context'].startswith( "code/snyk") and event_payload['state'] == 'pending':
-  # Report that code scan is in progress
-  print("Snyk code is in progress")
-  action(event_payload, "code-scan")
-else:
-  # Catch-all?
-  print("Skipping checks")
+# if event_payload['context'].startswith( "license/snyk") and event_payload['state']== 'pending':
+#   # Report that 
+#   print("Snyk Open source license check is in progress")
+#   action(event_payload, "oss-license-scan")
+# elif event_payload['context'].startswith( "security/snyk") and event_payload['state'] == 'pending':
+#   # Report that 
+#   print("Snyk Open source license check is in progress")
+#   action(event_payload, "sca-scan")
+# elif event_payload['context'].startswith( "code/snyk") and event_payload['state'] == 'pending':
+#   # Report that code scan is in progress
+#   print("Snyk code is in progress")
+#   action(event_payload, "code-scan")
+# else:
+#   # Catch-all?
+#   print("Skipping checks")
 
 
 
