ad3c12d5e4bc1a503119be8abc5dd5f7d769bcef
u-crew <129836422+u-crew@users.noreply.github.com>
2024-03-27 08:41:23 +1100

    debug 5

--- py-setup/snyk-catch.py
+++ py-setup/snyk-catch.py
@@ -2,14 +2,15 @@ import json
 from github_action_utils import event_payload
 
 # Get payload from the evnents check_run
-x = event_payload()
+
 event_payload = event_payload()
+print(event_payload)
 # Check for snyk Open source license in pending state
 if event_payload.context.startswith( "license/snyk") and event_payload.state == 'pending':
   # Report that 
   print("Snyk Open source license check is in progress")
   action(event_payload, "oss-license-scan")
-elif event_payload.context.startswith( "security/snyk") and event_payload.state == 'pending':
+elif event_payload['context'].startswith( "security/snyk") and event_payload.state == 'pending':
   # Report that 
   print("Snyk Open source license check is in progress")
   action(event_payload, "sca-scan")
